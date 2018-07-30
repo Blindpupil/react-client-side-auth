8b3bcaa266b988f00bc48036fc146fbe92bf3714
Cesar Martinez <cesar.martinez@globalgaming.com>
2018-07-30 17:13:59 +0200

    Action, CORS, and form post submit

--- client/src/components/authentication/Signup.js
+++ client/src/components/authentication/Signup.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
+import * as actions from '../../actions';
 
 class Sign_up extends Component {
+  onSubmit = (formProps) => {
+    this.props.signup(formProps);
+  };
+
   render() {
+    // grab the handleSubmit provided by redux-form
+    const { handleSubmit } = this.props;
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label htmlFor="">Email</label>
           <Field name="email" type="text"
@@ -17,9 +26,13 @@ class Sign_up extends Component {
                  component="input" autoComplete="none">
           </Field>
         </fieldset>
+        <button>Sign Up</button>
       </form>
     )
   }
 }
 
-export default reduxForm({form: 'signup'})(Sign_up);
+export default compose(
+  connect(null, actions), // High Order Components applied to this one
+  reduxForm({form: 'signup'})
+)(Sign_up);
